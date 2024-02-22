00fedb111fc3c0024781e575121fb16226752f0c
YashrajMalviya <yashrajmalviya296@gmail.com>
2024-02-22 14:27:16 +0530

    blog-tile nd newsCategoryTile

--- lib/views/home.dart
+++ lib/views/home.dart
@@ -1,4 +1,6 @@
+import 'package:daily_pulse_news/helper/data.dart';
 import 'package:flutter/material.dart';
+import 'package:daily_pulse_news/models/category_model.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -8,6 +10,14 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  List<CategoryModel> categories = [];
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    categories = getCategories();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -16,16 +26,34 @@ class _HomePageState extends State<HomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             Text("Daily "),
-            Text("Pulse",style: TextStyle(
-              color: Colors.purple
-            ),)
+            Text(
+              "Pulse",
+              style: TextStyle(color: Colors.purple),
+            )
           ],
         ),
         centerTitle: true,
         elevation: 0.0,
       ),
       body: Container(
-        child: ,
+        child: Column(
+          children: [
+            Container(
+              padding: EdgeInsets.symmetric(horizontal: 16),
+              height: 70,
+              child: ListView.builder(
+                  itemCount: categories.length,
+                  shrinkWrap: true,
+                  scrollDirection: Axis.horizontal,
+                  itemBuilder: (context, index) {
+                    return NewsCategoryTile(
+                      imageUrl: categories[index].imageUrl,
+                      categoryName: categories[index].categoryName,
+                    );
+                  }),
+            )
+          ],
+        ),
       ),
     );
   }
@@ -37,13 +65,63 @@ class NewsCategoryTile extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: (){
+
+      },
+      child: Container(
+        margin: EdgeInsets.only(right: 16),
+        child: Stack(
+          children: [
+            ClipRRect(
+              borderRadius: BorderRadius.circular(6),
+              child: Image.network(
+                imageUrl,
+                width: 120,
+                height: 60,
+                fit: BoxFit.cover,
+              ),
+            ),
+            Container(
+              alignment: Alignment.center,
+              width: 120,
+              height: 60,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(6),
+                color: Colors.black26,
+              ),
+
+              child: Text(categoryName,style: TextStyle(
+                color: Colors.white,
+                fontSize: 14,
+                fontWeight: FontWeight.w500
+              ),),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class BlogTile extends StatelessWidget {
+
+  final String imageUrl, title, desc;
+  const BlogTile({super.key, required this.imageUrl, required this.title, required this.desc});
+
+  @override
+  Widget build(BuildContext context) {
     return Container(
-      child: Stack(
+      child: Column(
         children: [
-          Image.network(imageUrl,width: 120, height: 60,)
+          Image.network(imageUrl),
+          Text(title),
+          Text(desc)
         ],
       ),
     );
   }
 }
+
+
 
